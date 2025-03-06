4849791eb2ce2e8299ec3d4fce9e67446ec05d30
Thomas Gorny <thomas.gorny@pm.me>
2025-03-06 23:43:01 +0100

    some tweaks in fbe shader

--- drafts/textures/shader/src/lib.rs
+++ drafts/textures/shader/src/lib.rs
@@ -68,29 +68,31 @@ pub fn fbm_shader_frag(
 	let st = uv * vec2(aspect, 1.0) * 3.0;
 	let time = *time;
 
-	let mut q = Vec2::splat(0.0);
-	q.x = simplex_noise_3d(st.extend(time)).fit1101();
-	q.y = simplex_noise_3d((st + vec2(1.0, 0.0)).extend(time)).fit1101();
-	// q.x = fbm(st + vec2(0.0, 0.0) * time);
-	// q.y = fbm(st + vec2(1.0, 0.0));
+	let q = vec2(
+		// simplex_noise_3d(st.extend(time)).fit1101(),
+		// simplex_noise_3d((st + vec2(1.0, 0.0)).extend(time)).fit1101(),
+		fbm(st),
+		fbm(st + vec2(10.0, 10.0)),
+	);
 
-	let mut r = Vec2::splat(0.0);
-	r.x = simplex_noise_3d((st + 1.0 * q + vec2(1.7, 9.2)).extend(0.15 * time)).fit1101();
-	r.y = simplex_noise_3d((st + 1.0 * q + vec2(8.3, 2.8)).extend(0.126 * time)).fit1101();
-	// r.x = fbm(st + 1.0 * q + vec2(1.7, 9.2) + vec2(0.15, 0.15) * time);
-	// r.y = fbm(st + 1.0 * q + vec2(8.3, 2.8) + vec2(0.126, 0.126) * time);
+	let r = vec2(
+		// simplex_noise_3d((st + 1.0 * q + vec2(1.7, 9.2)).extend(0.15 * time)).fit1101(),
+		// simplex_noise_3d((st + 1.0 * q + vec2(8.3, 2.8)).extend(0.126 * time)).fit1101(),
+		fbm(st + 1.1 * q + 0.15 * time),
+		fbm(st + 1.0 * q + 0.126 * time),
+	);
 
-	let f = simplex_noise_2d(st + r).fit1101();
-	// let f = fbm(st + r);
+	// let f = simplex_noise_2d(st + r).fit1101();
+	let f = fbm(st + r + time * 0.1);
 
 	let mut color = vec3(0.101961, 0.619608, 0.666667).lerp(
-		vec3(0.666667, 0.666667, 0.498039),
+		vec3(0.666667, 0.666667, 0.198039),
 		((f * f) * 4.0).clamp(0.0, 1.0),
 	);
 
 	color = color.lerp(vec3(0.0, 0.0, 0.164706), q.length().clamp(0.0, 1.0));
 
-	color = color.lerp(vec3(0.666667, 1.0, 1.0), r.x.clamp(0.0, 1.0));
+	color = color.lerp(vec3(0.666667, 1.0, 1.0), r.length().clamp(0.0, 1.0));
 
 	*frag_color = ((f * f * f + 0.6 * f * f + 0.5 * f) * color).extend(1.0);
 }
