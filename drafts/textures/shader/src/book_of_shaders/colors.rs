3fd00ac5ad97171877387b838d0705da26f2ba7a
Thomas Gorny <thomas.gorny@pm.me>
2025-03-23 14:19:15 +0100

    update color example

--- drafts/textures/shader/src/book_of_shaders/colors.rs
+++ drafts/textures/shader/src/book_of_shaders/colors.rs
@@ -1,13 +1,17 @@
+#![allow(unused_imports)]
+
+use core::f32::consts::TAU;
+
 use spirv_std::glam::{vec3, Vec2, Vec4};
 use spirv_std::num_traits::Float;
-use trivalibs_shaders::color::{hsl2rgb, rgb2hsl};
+use trivalibs_shaders::color::{hsv2rgb, rgb2hsl};
 // use crate::fbm;
 
 pub fn color_test(uv: Vec2, time: f32) -> Vec4 {
 	let c = vec3(uv.x, uv.y, 1.0);
-	let mut c_hsl = rgb2hsl(c);
-	// c_hsl.z = (c_hsl.z + (time + uv.x * 2.0).sin()) * 0.5;
-	let c_rgb = hsl2rgb(c_hsl);
-	c_rgb.extend(1.0);
-	c.extend(1.0)
+	let mut c_hsv = rgb2hsl(c);
+	c_hsv.z = (c_hsv.z + (time + uv.x * TAU * 6.0).sin()) * 0.5;
+	let c_rgb = hsv2rgb(c_hsv);
+	c_rgb.extend(1.0)
+	// c.extend(1.0)
 }
