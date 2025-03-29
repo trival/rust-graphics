797ff042d695f6a763870b5f59c9d585c3bd2847
Thomas Gorny <thomas.gorny@pm.me>
2025-03-29 08:54:21 +0100

    little improvment on circles

--- drafts/textures/shader/src/book_of_shaders/shapes.rs
+++ drafts/textures/shader/src/book_of_shaders/shapes.rs
@@ -141,7 +141,7 @@ pub fn shader_circles(st: Vec2, t: f32) -> Vec4 {
 	let c1 = (Vec2::splat(0.4) - st).length();
 	let c2 = (Vec2::splat(0.6) - st).length();
 
-	let line_count = 35.;
+	let line_count = 35.0.powf(2.0);
 	// let val = c1 + c2;
 	// let val = (c1 + c2) / 2.;
 	// let val = c1 - c2;
@@ -152,7 +152,7 @@ pub fn shader_circles(st: Vec2, t: f32) -> Vec4 {
 	// let val = c1.powf(c2);
 	// let val = c2.powf(c1);
 
-	let i = (val * line_count).floor();
+	let i = (val * line_count).powf(0.5).floor();
 
 	let c1 = Vec3::ZERO;
 	let c2 = Vec3::ONE;
@@ -160,8 +160,8 @@ pub fn shader_circles(st: Vec2, t: f32) -> Vec4 {
 	let center = vec2(0.5, 0.5) - st;
 	let angle = (center.y.atan2(center.x) + PI) / TAU;
 
-	let color = if i == 0.0 {
-		let line = circle_line(1.0, line_count, t, angle);
+	let color = if i < 6.0 {
+		let line = circle_line(6.0, line_count, t, angle);
 		c2.lerp(c1, line)
 	} else {
 		let line = circle_line(i, line_count, t, angle);
@@ -172,7 +172,7 @@ pub fn shader_circles(st: Vec2, t: f32) -> Vec4 {
 }
 
 fn circle_line(i: f32, line_count: f32, t: f32, angle: f32) -> f32 {
-	let v1 = hash(i as u32 * 2) * 0.5;
+	let v1 = hash(i as u32 * 3) * 0.5;
 	let v2 = hash(i as u32 + line_count as u32) * 0.5 + 0.5;
 
 	let s = if v2 > 0.75 { 1.0 } else { -1.0 };
