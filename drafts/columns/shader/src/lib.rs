1eadc1d0e7fc75c13bf2835d6e53a3a2854f1d1f
Thomas Gorny <thomas.gorny@pm.me>
2025-05-11 09:41:21 +0200

    first column draft

---
+++ drafts/columns/shader/src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![allow(unexpected_cfgs)]
+
+use spirv_std::glam::{vec3, Mat3, Mat4, Vec2, Vec3, Vec4};
+use spirv_std::spirv;
+
+#[spirv(vertex)]
+pub fn ground_vert(
+	position: Vec3,
+	uv: Vec2,
+	normal: Vec3,
+	#[spirv(uniform, descriptor_set = 0, binding = 0)] m_mat: &Mat4,
+	#[spirv(uniform, descriptor_set = 0, binding = 1)] n_mat: &Mat3,
+	#[spirv(uniform, descriptor_set = 0, binding = 2)] vp_mat: &Mat4,
+	#[spirv(position)] out_pos: &mut Vec4,
+	out_norm: &mut Vec3,
+	out_uv: &mut Vec2,
+) {
+	*out_pos = *vp_mat * *m_mat * position.extend(1.0);
+	*out_norm = *n_mat * normal;
+	*out_uv = uv;
+}
+
+#[spirv(fragment)]
+pub fn ground_frag(_in_norm: Vec3, in_uv: Vec2, out: &mut Vec4) {
+	let uv = in_uv * 40.0;
+	let uv = uv.fract();
+
+	let col = if uv.x < 0.05 || uv.y < 0.05 {
+		Vec3::splat(0.4)
+	} else {
+		vec3(in_uv.x, in_uv.y, 0.5)
+	};
+
+	*out = col.powf(2.2).extend(1.0);
+}
---
+++ drafts/columns/src/main.rs
@@ -0,0 +1,261 @@
+use std::f32::consts::PI;
+
+use trivalibs::{
+	common_utils::camera_controls::BasicFirstPersonCameraController,
+	map,
+	math::transform::Transform,
+	painter::{prelude::*, utils::input_state::InputState},
+	prelude::*,
+	rendering::{
+		camera::{CamProps, PerspectiveCamera},
+		mesh_geometry::{
+			face_normal,
+			utils::{vert_pos_uv, Vert3dUv},
+			FaceDataProps, MeshBufferType, MeshGeometry,
+		},
+		scene::SceneObject,
+		shapes::{cuboid::Cuboid, quad::Quad3D},
+		BufferedGeometry,
+	},
+};
+
+pub fn create_plane(width: f32, height: f32, normal: Vec3, center: Vec3) -> BufferedGeometry {
+	let plane: Quad3D<Vert3dUv> =
+		Quad3D::from_dimensions_center_f(width, height, normal, center, vert_pos_uv).into();
+
+	let mut geom = MeshGeometry::new();
+	geom.add_face4_data(plane.to_ccw_verts(), face_normal(plane.normal));
+
+	geom.to_buffered_geometry_by_type(MeshBufferType::FaceNormals)
+}
+
+pub fn create_balk_form(width: f32, height: f32, length: f32) -> BufferedGeometry {
+	let bbox = Cuboid::box_at(Vec3::ZERO, width, height, length);
+
+	let mut geom = MeshGeometry::new();
+
+	let face_data = |normal: Vec3, section: usize| FaceDataProps {
+		normal: Some(normal),
+		section: Some(section),
+		data: None,
+	};
+
+	let left = bbox.left_face_f(|pos, uvw| vert_pos_uv(pos, vec2(1.0 - uvw.z, uvw.y)));
+	geom.add_face4_data(left.to_ccw_verts(), face_data(left.normal, 2));
+
+	let right = bbox.right_face_f(|pos, uvw| vert_pos_uv(pos, vec2(uvw.z, uvw.y)));
+	geom.add_face4_data(right.to_ccw_verts(), face_data(right.normal, 3));
+
+	let bottom = bbox.bottom_face_f(|pos, uvw| vert_pos_uv(pos, vec2(uvw.x, uvw.z)));
+	geom.add_face4_data(bottom.to_ccw_verts(), face_data(bottom.normal, 5));
+
+	geom.to_buffered_geometry_by_type(MeshBufferType::FaceNormals)
+}
+
+pub fn create_column_form(width: f32, height: f32) -> BufferedGeometry {
+	let bbox = Cuboid::box_at(Vec3::ZERO, width, height, width);
+
+	let mut geom = MeshGeometry::new();
+
+	let face_data = |normal: Vec3, section: usize| FaceDataProps {
+		normal: Some(normal),
+		section: Some(section),
+		data: None,
+	};
+
+	let front = bbox.front_face_f(|pos, uvw| vert_pos_uv(pos, vec2(uvw.x, uvw.y)));
+	geom.add_face4_data(front.to_ccw_verts(), face_data(front.normal, 0));
+
+	let back = bbox.back_face_f(|pos, uvw| vert_pos_uv(pos, vec2(1.0 - uvw.x, uvw.y)));
+	geom.add_face4_data(back.to_ccw_verts(), face_data(back.normal, 1));
+
+	let left = bbox.left_face_f(|pos, uvw| vert_pos_uv(pos, vec2(1.0 - uvw.z, uvw.y)));
+	geom.add_face4_data(left.to_ccw_verts(), face_data(left.normal, 2));
+
+	let right = bbox.right_face_f(|pos, uvw| vert_pos_uv(pos, vec2(uvw.z, uvw.y)));
+	geom.add_face4_data(right.to_ccw_verts(), face_data(right.normal, 3));
+
+	geom.to_buffered_geometry_by_type(MeshBufferType::FaceNormals)
+}
+
+struct App {
+	cam: PerspectiveCamera,
+	vp_mat: UniformBuffer<Mat4>,
+	canvas: Layer,
+
+	input: InputState,
+	cam_controller: BasicFirstPersonCameraController,
+}
+
+impl CanvasApp<()> for App {
+	fn init(p: &mut Painter) -> Self {
+		let shade = p
+			.shade(&[Float32x3, Float32x3, Float32x2])
+			.with_uniforms(&[
+				UNIFORM_BUFFER_VERT,
+				UNIFORM_BUFFER_VERT,
+				UNIFORM_BUFFER_VERT,
+			])
+			.create();
+		load_vertex_shader!(shade, p, "../shader/ground_vert.spv");
+		load_fragment_shader!(shade, p, "../shader/ground_frag.spv");
+
+		let cam = PerspectiveCamera::create(CamProps {
+			fov: Some(0.6),
+			translation: Some(vec3(0.0, 3.0, 15.0)),
+			..default()
+		});
+
+		let ground_form = p
+			.form(&create_plane(100.0, 100.0, Vec3::Y, Vec3::ZERO))
+			.create();
+		let g_m_mat = p.uniform_const_mat4(Mat4::IDENTITY);
+		let g_n_mat = p.uniform_const_mat3(Mat3::IDENTITY);
+		let ground_shape = p
+			.shape(ground_form, shade)
+			.with_uniforms(map! {
+				0 => g_m_mat,
+				1 => g_n_mat
+			})
+			.create();
+
+		let column_form = p.form(&create_column_form(3., 30.)).create();
+		let mut column_transforms = vec![];
+		for i in -2..=2 {
+			column_transforms.push(Transform::from_translation(vec3(
+				-25.,
+				15.,
+				i as f32 * 25. / 2.,
+			)));
+			column_transforms.push(Transform::from_translation(vec3(
+				25.,
+				15.,
+				i as f32 * 25. / 2.,
+			)));
+			column_transforms.push(Transform::from_translation(vec3(
+				i as f32 * 25. / 2.,
+				15.,
+				-25.,
+			)));
+			column_transforms.push(Transform::from_translation(vec3(
+				i as f32 * 25. / 2.,
+				15.,
+				25.,
+			)));
+		}
+		let column_instances = column_transforms
+			.iter()
+			.map(|t| {
+				let m_mat = t.model_mat();
+				let n_mat = t.model_normal_mat();
+				let u_m_mat = p.uniform_const_mat4(m_mat);
+				let u_n_mat = p.uniform_const_mat3(n_mat);
+				InstanceUniforms {
+					uniforms: map! {
+						0 => u_m_mat,
+						1 => u_n_mat
+					},
+					..default()
+				}
+			})
+			.collect();
+		let column_shape = p
+			.shape(column_form, shade)
+			.with_instances(column_instances)
+			.create();
+
+		let balk_form = p.form(&create_balk_form(3., 9., 50.)).create();
+		let mut balk_transforms = vec![];
+		for i in -2..=2 {
+			balk_transforms.push(Transform::from_translation(vec3(
+				i as f32 * 25. / 2.,
+				30.,
+				0.,
+			)));
+			balk_transforms.push(
+				Transform::from_translation(vec3(0., 30., i as f32 * 25. / 2.))
+					.with_rotation(Quat::from_rotation_y(PI / 2.)),
+			);
+		}
+		let balk_instances = balk_transforms
+			.iter()
+			.map(|t| {
+				let m_mat = t.model_mat();
+				let n_mat = t.model_normal_mat();
+				let u_m_mat = p.uniform_const_mat4(m_mat);
+				let u_n_mat = p.uniform_const_mat3(n_mat);
+				InstanceUniforms {
+					uniforms: map! {
+						0 => u_m_mat,
+						1 => u_n_mat
+					},
+					..default()
+				}
+			})
+			.collect();
+		let balk_shape = p
+			.shape(balk_form, shade)
+			.with_instances(balk_instances)
+			.create();
+
+		let vp_mat = p.uniform_mat4();
+
+		let canvas = p
+			.layer()
+			.with_shapes(vec![ground_shape, column_shape, balk_shape])
+			.with_clear_color(wgpu::Color {
+				r: 0.5,
+				g: 0.6,
+				b: 0.7,
+				a: 1.0,
+			})
+			.with_uniforms(map! {
+				2 => vp_mat.uniform(),
+			})
+			.with_multisampling()
+			.with_depth_test()
+			.create();
+
+		Self {
+			cam,
+			canvas,
+			vp_mat,
+			input: default(),
+			cam_controller: BasicFirstPersonCameraController::new(1.0, 3.0),
+		}
+	}
+
+	fn resize(&mut self, _p: &mut Painter, width: u32, height: u32) {
+		self.cam.set_aspect_ratio(width as f32 / height as f32);
+		self.cam_controller.set_screen_size(width, height);
+	}
+
+	fn update(&mut self, p: &mut Painter, tpf: f32) {
+		self
+			.cam_controller
+			.update_camera(&mut self.cam, &self.input, tpf);
+
+		self.vp_mat.update(p, self.cam.view_proj_mat());
+
+		p.request_next_frame();
+	}
+
+	fn render(&self, p: &mut Painter) -> std::result::Result<(), wgpu::SurfaceError> {
+		p.paint_and_show(self.canvas)
+	}
+
+	fn event(&mut self, e: Event<()>, _p: &mut Painter) {
+		self.input.process_event(e);
+	}
+}
+
+pub fn main() {
+	App::create()
+		.config(AppConfig {
+			show_fps: true,
+			use_vsync: true,
+			keep_window_dimensions: true,
+			..default()
+		})
+		.start();
+}
