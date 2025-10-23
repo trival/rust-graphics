b59191a3f230c98c5df6658a0fb757de323bcfab
Thomas Gorny <thomas.gorny@pm.me>
2025-10-23 22:11:25 +0200

    add a minimal template to copy when starting new drafts

---
+++ drafts/template/shader/src/lib.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![allow(unexpected_cfgs)]
+
+use spirv_std::glam::{Vec2, Vec4, vec4};
+use spirv_std::spirv;
+
+#[spirv(fragment)]
+pub fn main(_coord: Vec2, out: &mut Vec4) {
+	*out = vec4(1.0, 0.0, 0.0, 1.0);
+}
